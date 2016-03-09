d84234b805e08f4de7a87f1995aa2773b729ac4f
Austin W Smith <auwsmit@gmail.com>
2016-03-09 06:01:55 -0800

    compatible with neovim for unix! yaaaay.

--- UnixSetup.sh
+++ UnixSetup.sh
@@ -1,41 +1,62 @@
 #!/bin/bash
-# Makes a symbolic link to this vimrc in your home directory
-# Puts any old vimrcs in a folder "dotfiles_backup"
+# A dotfiles setup script. Also not pretty.
+# Backups of any potentially replaced files are located at ~/dotfiles_backup
 
-# delete previous symbolic links if they exist
+# In this order:
+# - Delete previous symbolic links if they exist.
+# - Backup the previous config if it exists.
+# - Make symbolic links.
+mkdir ~/dotfiles_backup
 if [ -L ~/.vim ] ; then
   rm ~/.vim
 fi
+if [ -e ~/.vim ] ; then
+  mv -i ~/.vim ~/dotfiles_backup
+fi
+ln -s -d $(pwd)/vimconfig ~/.vim
+
+# nvim
+if [ -e ~/.config ] ; then
+  mv -i ~/.config ~/dotfiles_backup
+fi
+mkdir ~/.config
+if [ -L ~/.config/nvim ] ; then
+  rm ~/.config/nvim
+fi
+ln -s -d $(pwd)/vimconfig ~/.config/nvim
+if [ -L $(pwd)/init.vim ] ; then
+  rm $(pwd)/init.vim
+fi
+ln -s ~/.config/nvim/init.vim $(pwd)/vimconfig/vimrc
+
 if [ -L ~/.bashrc ] ; then
   rm ~/.bashrc
 fi
+if [ -e ~/.bashrc ] ; then
+  mv -i ~/.bashrc ~/dotfiles_backup
+fi
+ln -s $(pwd)/.bashrc ~/.bashrc
+
 if [ -L ~/.bash_profile ] ; then
   rm ~/.bash_profile
 fi
+if [ -e ~/.bash_profile ] ; then
+  mv -i ~/.bash_profile ~/dotfiles_backup
+fi
+ln -s $(pwd)/.bash_profile ~/.bash_profile
+
 if [ -L ~/.profile ] ; then
   rm ~/.profile
 fi
-
-# backup the previous config if it exists
-if [ -e ~/.vim ] ; then
-  mkdir ~/dotfiles_backup
-  mv ~/.vim ~/dotfiles_backup
-  if [ -e ~/.bashrc ] ; then
-    mv ~/.bashrc ~/dotfiles_backup
-  fi
-  if [ -e ~/.bash_profile ] ; then
-    mv ~/.bash_profile ~/dotfiles_backup
-  fi
-  if [ -e ~/.profile ] ; then
-    mv ~/.profile ~/dotfiles_backup
-  fi
+if [ -e ~/.profile ] ; then
+  mv -i ~/.profile ~/dotfiles_backup
 fi
+ln -s $(pwd)/.profile ~/.profile
 
-# symbolic links
-ln -s -d $(pwd)/vimconfig ~/.vim
-ln -s -d $(pwd)/.bashrc ~/.bashrc
-ln -s -d $(pwd)/.bash_profile ~/.bash_profile
-ln -s -d $(pwd)/.profile ~/.profile
+# check for stray vimrc
+if [ -e ~/.vimrc ] ; then
+  mv -i ~/.vimrc ~/dotfiles_backup
+fi
 
 # custom configs like remapping Caps Lock to Escape
 source ~/.bashrc
