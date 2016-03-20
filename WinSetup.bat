06f69ca2d6a8831ab806e74cb0f5682027fbbcf8
Austin W Smith <auwsmit@gmail.com>
2016-03-20 12:11:12 -0700

    added vinegar, disabled gutentags, and more

--- WinSetup.bat
+++ WinSetup.bat
@@ -1,35 +1,42 @@
 @echo off
+
 :: VARIABLES
 set home_dir=%userprofile%
 :: %~dp0 expands to the full path of this file
 set repo_dir=%~dp0
 
 :: In this order:
-:: - Delete previous symbolic links if they exist.
-:: - Backup the previous config if it exists.
-:: - Make symbolic links.
+:: 1. Delete previous symbolic link if it exists.
+::   - In order to preserve files:
+::     - delete symbolic files with DEL
+::     - delete symbolic directories with RMDIR
+:: 2. Backup the previous config if it exists.
+:: 3. Make symbolic links.
+
+:: Backup folder
 mkdir "%home_dir%\dotfiles_backup"
 
+:: Regular Vim
 dir "%home_dir%\vimfiles" | find "<SYMLINK>" && (
-	rmdir /s /q "%home_dir%\vimfiles"
-)
+        rmdir "%home_dir%\vimfiles"
+        )
 if exist { "%home_dir%\vimfiles" } (
-	move /-y "%home_dir%\vimfiles" "%home_dir%\dotfiles_backup\vimfiles"
-)
+        move /-y "%home_dir%\vimfiles" "%home_dir%\dotfiles_backup\vimfiles"
+        )
 mklink /d  "%home_dir%\vimfiles" "%repo_dir%\vimconfig"
 
-:: neovim
+:: Neovim
 dir "%userprofile%\AppData\Local\nvim" | find "<SYMLINK>" && (
-	del "%userprofile%\AppData\Local\nvim"
-)
+        rmdir "%userprofile%\AppData\Local\nvim"
+        )
 mklink /d "%userprofile%\AppData\Local\nvim" "%repo_dir%\vimconfig"
 dir "%userprofile%\AppData\Local\nvim\init.vim" | find "<SYMLINK>" && (
-	del "%userprofile%\AppData\Local\init.vim"
-)
+        del "%userprofile%\AppData\Local\init.vim"
+        )
 if exist { "%userprofile%\AppData\Local\nvim\init.vim" } (
-	move /-y "%userprofile%\AppData\Local\nvim\init.vim" "%home_dir%\dotfiles_backup\"
-)
+        move /-y "%userprofile%\AppData\Local\nvim\init.vim" "%home_dir%\dotfiles_backup\"
+        )
 mklink "%userprofile%\AppData\Local\nvim\init.vim" "%repo_dir%\vimconfig\vimrc"
 
 :: run compiled autohotkey script to remap Caps Lock to Escape
-%repo_dir%\CapsToEscape
+"%repo_dir%\CapsToEscape"
