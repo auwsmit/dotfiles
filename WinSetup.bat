c280773437352b85b8aab620e2760ef079b776ed
Austin W Smith <auwsmit@gmail.com>
2016-03-28 03:49:24 -0700

    hopefully ironed out symlink garbage

--- WinSetup.bat
+++ WinSetup.bat
@@ -17,26 +17,25 @@ set repo_dir=%~dp0
 mkdir "%home_dir%\dotfiles_backup"
 
 :: Regular Vim
-dir "%home_dir%\vimfiles" | find "<SYMLINK>" && (
-	echo what
-        rmdir "%home_dir%\vimfiles"
-        )
+dir "%home_dir%\vimfiles*" | find "<SYMLINKD>" && (
+    rmdir "%home_dir%\vimfiles"
+)
 if exist { "%home_dir%\vimfiles" } (
-        move /-y "%home_dir%\vimfiles" "%home_dir%\dotfiles_backup\vimfiles"
-        )
+    move /-y "%home_dir%\vimfiles" "%home_dir%\dotfiles_backup\vimfiles"
+)
 mklink /d  "%home_dir%\vimfiles" "%repo_dir%\vimconfig"
 
 :: Neovim
-dir "%userprofile%\AppData\Local\nvim" | find "<SYMLINK>" && (
-        rmdir "%userprofile%\AppData\Local\nvim"
-        )
+dir "%userprofile%\AppData\Local\nvim*" | find "<SYMLINKD>" && (
+    rmdir "%userprofile%\AppData\Local\nvim"
+)
 mklink /d "%userprofile%\AppData\Local\nvim" "%repo_dir%\vimconfig"
 dir "%userprofile%\AppData\Local\nvim\init.vim" | find "<SYMLINK>" && (
-        del "%userprofile%\AppData\Local\init.vim"
-        )
+    del "%userprofile%\AppData\Local\nvim\init.vim"
+)
 if exist { "%userprofile%\AppData\Local\nvim\init.vim" } (
-        move /-y "%userprofile%\AppData\Local\nvim\init.vim" "%home_dir%\dotfiles_backup\"
-        )
+    move /-y "%userprofile%\AppData\Local\nvim\init.vim" "%home_dir%\dotfiles_backup\"
+)
 mklink "%userprofile%\AppData\Local\nvim\init.vim" "%repo_dir%\vimconfig\vimrc"
 
 :: run compiled autohotkey script to remap Caps Lock to Escape
