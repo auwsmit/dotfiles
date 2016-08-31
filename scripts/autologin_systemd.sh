b174356ee20eebf3309dfc1466d4608ab912b587
Austin W Smith <auwsmit@gmail.com>
2016-08-31 13:18:57 -0700

    added description of script

--- scripts/autologin_systemd.sh
+++ scripts/autologin_systemd.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# This script will cause virtual terminal 1 to
+# automatically login for systems running systemd.
 mkdir -p /etc/systemd/system/getty@tty1.service.d/
 echo '[Service]
 ExecStart=
