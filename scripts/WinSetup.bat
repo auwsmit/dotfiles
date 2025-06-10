61d3254f3bd31365d5f50435846178a99b87a88f
Austin W Smith <auwsmit@gmail.com>
2025-06-10 01:17:24 -0700

    updated windows setup script

--- scripts/WinSetup.bat
+++ scripts/WinSetup.bat
@@ -1,4 +1,7 @@
-@echo off
+:: @echo off
+:: This script create symlinks from my dotfiles repo to the proper places
+:: Also stores any old config files in a backup folder just in case
+:: ps holy shit batch scripting is awful
 
 :: VARIABLES
 :: %~dp0 expands to the full path of this file
@@ -6,37 +9,36 @@
 set repo_dir=%~dp0..\
 set home_dir=%userprofile%\
 
-:: In this order:
-:: 1. Delete previous symbolic link if it exists.
-::   - In order to preserve files:
-::     - delete symbolic files with DEL
-::     - delete symbolic directories with RMDIR
-:: 2. Backup the previous config if it exists.
-:: 3. Make symbolic links.
-
-:: TODO: make a function to make this easier to modify
-
 :: Backup folder
+if exist { "%home_dir%%~1" } () else (
 mkdir "%home_dir%old_dotfiles"
+)
 
 :: Regular vim
-dir "%home_dir%vimfiles*" | find "<SYMLINKD>" && (
-    rmdir "%home_dir%vimfiles"
-)
-if exist { "%home_dir%vimfiles" } (
-    move /-y "%home_dir%vimfiles" "%home_dir%old_dotfiles\vimfiles"
-)
+CALL :Backup_Old_Files "" "vimfiles"
 mklink /d  "%home_dir%vimfiles" "%repo_dir%vimconfig"
 
 :: Neovim
-dir "%home_dir%AppData\Local\nvim*" | find "<SYMLINKD>" && (
-    rmdir "%home_dir%AppData\Local\nvim"
-)
+CALL :Backup_Old_Files "AppData\Local\" "nvim"
+CALL :Backup_Old_Files "AppData\Local\" "nvim-data"
 mklink /d "%home_dir%AppData\Local\nvim" "%repo_dir%vimconfig"
-dir "%home_dir%AppData\Local\nvim\init.vim" | find "<SYMLINK>" && (
-    del "%home_dir%AppData\Local\nvim\init.vim"
+mklink /d "%home_dir%AppData\Local\nvim-data" "%repo_dir%vimconfig"
+mklink "%repo_dir%vimconfig\init.vim" "%repo_dir%vimconfig\vimrc"
+
+:: delete backup folder if nothing was backed up
+dir /b /s /a "%home_dir%old_dotfiles" | findstr .>nul || (
+    rmdir "%home_dir%old_dotfiles"
 )
-if exist { "%home_dir%AppData\Local\nvim\init.vim" } (
-    move /-y "%home_dir%AppData\Local\nvim\init.vim" "%home_dir%old_dotfiles\"
+
+:: FUNCTIONS
+EXIT /B %ERRORLEVEL%
+:Backup_Old_Files
+set backup_dir=%~1
+set backup_file=%~2
+dir "%home_dir%%backup%*" | find "<SYMLINKD>" && (
+    rmdir "%home_dir%%backup_dir%%backupfile%"
 )
-mklink "%home_dir%AppData\Local\nvim\init.vim" "%repo_dir%vimconfig\vimrc"
+if exist { "%home_dir%%backup%" } (
+    move /-y "%home_dir%%backup_dir%%backupfile%" "%home_dir%old_dotfiles\%backup_file%"
+)
+EXIT /B 0
