7bac12cbf76372611e7f0660598b20e7f0b400f5
Austin W Smith <auwsmit@gmail.com>
2025-06-23 15:52:10 -0700

    updated Unix setup script
    should work for neovim's data folder now. also disabled all the decade
    old stuff that was mostly for my very ancient and slow old laptop

--- scripts/UnixSetup.sh
+++ scripts/UnixSetup.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
 # A dotfiles setup script.
 
-# === VARIABLES ==================
+# === VARIABLES ===
 
 # YOUR USERNAME GOES HERE:
-username=root
-# (if you don't know, run whoami in the terminal)
+username=
 
 # Any of your previous config files will be found here.
 #           V  V  V  V  V  V
 backup_dir=~/.old_dotfiles
 repo_dir=$(cd .. && pwd)
 
-# === FUNCTIONS ==================
+# === FUNCTIONS ===
 
 # without this, folders would be owned by root
 my_mkdir() {
@@ -20,75 +19,76 @@ my_mkdir() {
     chown $username:$username $1
 }
 
-# $1 thing to backup,
-# $2 path/name of file to link,
+# $1 target of the link
+# $2 thing to link and backup,
 # $3 path for backup
 make_link_and_backup () {
   # delete previous symbolic link if it exists
-  if [ -L $1 ]; then rm $1; fi
+  if [ -L $2 ]; then rm $2; fi
   # backup the previous file/folder if it exists
-  if [ -e $1 ]; then
+  if [ -e $2 ]; then
       my_mkdir $3
-      mv -i $1 $3
+      mv -i $2 $3
   fi
   # make symbolic link
   # for folder
-  if [ -d $1 ]; then ln -s -d $2 $1; fi
+  if [ -d $2 ]; then ln -s -d $1 $2; fi
   # for file
-  if [ ! -d $1 ]; then ln -s $2 $1; fi
-  chown -h $username:$username $1
+  if [ ! -d $2 ]; then ln -s $1 $2; fi
+  chown -h $username:$username $2
 }
 
-# === MAIN CODE ==================
+# === MAIN CODE ===
 
-# make backup/old config directory
+# make backup directory for already existing files
 my_mkdir $backup_dir
 
 # vim
-make_link_and_backup \
-  ~/.vim $repo_dir/vimconfig $backup_dir
-
-# neovim
-my_mkdir ~/.config
-make_link_and_backup \
-  ~/.config/nvim $repo_dir/vimconfig $backup_dir/.config
-if [ -L $repo_dir/init.vim ] ; then
-  rm $repo_dir/init.vim
-fi
-if [ ! -e ~/.config/nvim/init.vim ] ; then
-  ln -s $repo_dir/vimconfig/vimrc ~/.config/nvim/init.vim
-fi
-
-# .bashrc
-make_link_and_backup \
-  ~/.bashrc $repo_dir/.bashrc $backup_dir
-
-# .bash_profile redirects to .bashrc
-make_link_and_backup \
-  ~/.bash_profile $repo_dir/.bashrc $backup_dir
-
-# .xinitrc
-make_link_and_backup \
-  ~/.xinitrc $repo_dir/.xinitrc $backup_dir
-
-# .xprofile
-make_link_and_backup \
-  ~/.xprofile $repo_dir/.xprofile $backup_dir
-
-# lilyterm
-my_mkdir ~/.config/lilyterm
-make_link_and_backup \
-  ~/.config/lilyterm/default.conf $repo_dir/lilyterm.conf $backup_dir/lilyterm
-
-# .xbindkeysrc
-make_link_and_backup \
-  ~/.xbindkeysrc $repo_dir/.xbindkeysrc $backup_dir
-
+make_link_and_backup $repo_dir/vimconfig ~/.vim $backup_dir
 # check for stray vimrc
 if [ -e ~/.vimrc ]; then
   mv -i ~/.vimrc $backup_dir
 fi
 
-# Disable pcspkr module (the horrid beeping)
-# rmmod -s pcspkr >> /dev/null 2>&1
-# echo 'install pcspkr /bin/false' > /etc/modprobe.d/aws_nobeep
+
+# neovim
+my_mkdir ~/.config
+make_link_and_backup $repo_dir/vimconfig ~/.config/nvim           $backup_dir/.config
+make_link_and_backup $repo_dir/vimconfig ~/.local/share/nvim-data $backup_dir/.local/share
+# since we're already making a link for .config/nvim,
+# just make a fake init.vim in the repo
+if [ -L $repo_dir/init.vim ] ; then
+  rm $repo_dir/init.vim
+fi
+if [ ! -e ~/.config/nvim/init.vim ] ; then
+  ln -s $repo_dir/vimconfig/vimrc $repo_dir/vimconfig/init.vim
+fi
+
+# .bashrc
+make_link_and_backup $repo_dir/.bashrc ~/.bashrc $backup_dir
+
+# like a decade out of date, will probably delete
+# # .bash_profile redirects to .bashrc
+# make_link_and_backup \
+#   ~/.bash_profile $repo_dir/.bashrc $backup_dir
+# .bash_profile redirects to .bashrc
+
+# make_link_and_backup \
+#   ~/.bash_profile $repo_dir/.bashrc $backup_dir
+
+# # .xinitrc
+# make_link_and_backup \
+#   ~/.xinitrc $repo_dir/.xinitrc $backup_dir
+
+# # .xprofile
+# make_link_and_backup \
+#   ~/.xprofile $repo_dir/.xprofile $backup_dir
+
+# # lilyterm
+# my_mkdir ~/.config/lilyterm
+# make_link_and_backup \
+#   ~/.config/lilyterm/default.conf $repo_dir/lilyterm.conf $backup_dir/lilyterm
+
+# # .xbindkeysrc
+# make_link_and_backup \
+#   ~/.xbindkeysrc $repo_dir/.xbindkeysrc $backup_dir
