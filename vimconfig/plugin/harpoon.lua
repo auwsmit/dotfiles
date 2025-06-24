8ff1bbc3a9e8bccd88ae7085e960b19bf22d8741
Austin W Smith <auwsmit@gmail.com>
2025-06-23 17:30:15 -0700

    in case harpoon isn't installed

--- vimconfig/plugin/harpoon.lua
+++ vimconfig/plugin/harpoon.lua
@@ -1,14 +1,18 @@
-local harpoon = require("harpoon")
+local ok, mod = pcall(require, 'nvim-treesitter')
 
--- REQUIRED
-harpoon:setup()
--- REQUIRED
+if ok then
+    local harpoon = require("harpoon")
 
-vim.keymap.set("n", "<space>a", function() harpoon:list():add() end)
-vim.keymap.set("n", "<c-e>",    function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
-vim.keymap.set("n", "<C-1>",    function() harpoon:list():select(1) end)
-vim.keymap.set("n", "<C-2>",    function() harpoon:list():select(2) end)
-vim.keymap.set("n", "<C-3>",    function() harpoon:list():select(3) end)
-vim.keymap.set("n", "<C-4>",    function() harpoon:list():select(4) end)
-vim.keymap.set("n", "<C-5>",    function() harpoon:list():prev() end)
-vim.keymap.set("n", "<C-6>",    function() harpoon:list():next() end)
+    -- REQUIRED
+    harpoon:setup()
+    -- REQUIRED
+
+    vim.keymap.set("n", "<space>a", function() harpoon:list():add() end)
+    vim.keymap.set("n", "<c-e>",    function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
+    vim.keymap.set("n", "<C-1>",    function() harpoon:list():select(1) end)
+    vim.keymap.set("n", "<C-2>",    function() harpoon:list():select(2) end)
+    vim.keymap.set("n", "<C-3>",    function() harpoon:list():select(3) end)
+    vim.keymap.set("n", "<C-4>",    function() harpoon:list():select(4) end)
+    vim.keymap.set("n", "<C-5>",    function() harpoon:list():prev() end)
+    vim.keymap.set("n", "<C-6>",    function() harpoon:list():next() end)
+end
