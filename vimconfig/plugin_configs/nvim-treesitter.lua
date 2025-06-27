ef4d3f9e6ffcab834dffbaf945641a1d6de4f8ca
Austin W Smith <auwsmit@gmail.com>
2025-06-27 15:44:11 -0700

    blah

--- vimconfig/plugin_configs/nvim-treesitter.lua
+++ vimconfig/plugin_configs/nvim-treesitter.lua
@@ -1,5 +1,5 @@
--- require 'nvim-treesitter.configs'.setup {
---     ensure_installed = { "lua", "python", "c", "cpp" },
---     highlight = { enable = true },
---     indent = { enable = true },
--- }
+require 'nvim-treesitter.configs'.setup {
+    ensure_installed = { "lua", "python", "c", "cpp" },
+    highlight = { enable = true },
+    indent = { enable = true },
+}
