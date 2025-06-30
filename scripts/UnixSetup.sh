b0b315e1d85b485a02c95347626143e1957ae0d8
auwsmit <auwsmit@gmail.com>
2025-06-29 21:37:30 -0700

    updated setup scripts

--- scripts/UnixSetup.sh
+++ scripts/UnixSetup.sh
@@ -3,26 +3,30 @@
 
 # === VARIABLES ===
 
-read -p "Enter username: " username
+username="$(whoami)"
+if [ $username == "root" ]; then
+  read -p "Enter username: " username
+  home=/home/$username
+else
+  home=~
+fi
 
-# TODO fix edge case where /home/ isnt the home directory
-# home=~
-home=/home/$username
-# Any of previous config files will be found here.
+# Any previous config files will be found here.
 #           V  V  V  V  V  V
 backup_dir=$home/.old_dotfiles
-repo_dir=$(cd .. && pwd)
+script_dir="$(dirname "$(realpath "$0")")"
+repo_dir=$script_dir/..
 
 # === FUNCTIONS ===
 
-# without this, folders would be owned by root
+# only needed if script is ran as root
 my_mkdir() {
   mkdir -p $1
   chown $username:$username $1
 }
 
 # $1 target of the link
-# $2 thing to link and backup,
+# $2 file to link and backup,
 # $3 path for backup
 make_link_and_backup () {
   # delete previous symbolic link if it exists
@@ -46,10 +50,21 @@ make_link_and_backup () {
 
 # === MAIN CODE ===
 
-# TODO move old dotfiles into backup folder
+# check if home directory is valid
+if [ ! -e $home ]; then
+  echo "Invalid home directory. Edit the script and run again."
+  exit 1
+fi
 
 # make backup directory for already existing files
-my_mkdir $backup_dir
+if [ -e $backup_dir ]; then
+  # backup any previously existing old_dotfiles
+  mv $backup_dir $home/.older_dotfiles
+  my_mkdir $backup_dir
+  mv $home/.older_dotfiles $backup_dir
+else
+  my_mkdir $backup_dir
+fi
 
 # vim
 make_link_and_backup $repo_dir/vimconfig $home/.vim $backup_dir
@@ -63,11 +78,6 @@ my_mkdir $home/.config
 my_mkdir $home/.local/share
 make_link_and_backup $repo_dir/vimconfig $home/.config/nvim      $backup_dir/.config
 make_link_and_backup $repo_dir/vimconfig $home/.local/share/nvim $backup_dir/.local/share
-# since we're already making a link for .config/nvim,
-# just make a fake init.vim in the repo
-if [ -L $repo_dir/init.vim ] ; then
-  rm $repo_dir/init.vim
-fi
 if [ ! -e $home/.config/nvim/init.vim ] ; then
   ln -s $repo_dir/vimconfig/vimrc $repo_dir/vimconfig/init.vim
 fi
@@ -77,4 +87,7 @@ make_link_and_backup $repo_dir/.bashrc $home/.bashrc $backup_dir
 # .inputrc
 make_link_and_backup $repo_dir/.inputrc $home/.inputrc $backup_dir
 
-# TODO if backup folder is empty, delete it
+# if backup folder is empty, delete it
+if [ -z "$(ls -A $backup_dir)" ]; then
+  rmdir $backup_dir
+fi
--- scripts/WinSetup.bat
+++ scripts/WinSetup.bat
@@ -1,65 +1,79 @@
-:: @echo off
-:: This script create symlinks from my dotfiles repo to the proper places
-:: Also stores any old config files in a backup folder just in case
+REM @echo off
+REM This script create symlinks from my dotfiles repo to the proper places
+REM Also stores any old config files in a backup folder just in case
 
-:: Any previous config files will be found here:
-::  V  V  V  V  V  V  V  V  V  V
-:: C:\Users\username\old_dotfiles
+REM Any previous config files will be found here:
+REM  V  V  V  V  V  V  V  V  V  V
+REM C:\Users\username\old_dotfiles
 
-:: VARIABLES
-:: %~dp0 expands to the full path of this file
-:: also ends with a \ unlike environment variables...
+REM VARIABLES
+REM =========
+
+REM %~dp0 expands to the full path of this file
+REM also ends with a \ unlike environment variables...
 set repo_dir=%~dp0..\
 set home_dir=%userprofile%\
 set backup_dir=%home_dir%old_dotfiles\
 
-:: TODO move old backup folder into backup folder
+REM =========
+REM MAIN CODE
+REM =========
 
-:: create backup folder for already existing dotfiles
-if exist { "%home_dir%%~1" } () else (
+REM make backup folder for already existing files
+if exist  "%backup_dir%" (
+    REM backup any previously existing old_dotfiles
+    move "%backup_dir%" "%home_dir%older_dotfiles"
+    mkdir "%backup_dir"
+    move "%home_dir%older_dotfiles" "%backup_dir%"
+) else (
 mkdir "%backup_dir%"
 )
 
-
-:: Vim
+REM Vim
 CALL :Backup "%home_dir%" "vimfiles" ""
 mklink /d  "%home_dir%vimfiles" "%repo_dir%vimconfig"
 
-:: Vim in git bash
+REM Vim in git bash
 CALL :Backup "%home_dir%" ".vim" ""
 mklink /d  "%home_dir%.vim" "%repo_dir%vimconfig"
 
-:: Neovim
-CALL :Backup "%homedir%AppData\Local\" "nvim" "AppData\Local"
-CALL :Backup "%homedir%AppData\Local\" "nvim-data" "AppData\Local"
+REM Neovim
+CALL :Backup "%home_dir%AppData\Local\" "nvim" "AppData\Local"
+CALL :Backup "%home_dir%AppData\Local\" "nvim-data" "AppData\Local"
 mklink /d "%home_dir%AppData\Local\nvim" "%repo_dir%vimconfig"
 mklink /d "%home_dir%AppData\Local\nvim-data" "%repo_dir%vimconfig"
 mklink "%repo_dir%vimconfig\init.vim" "%repo_dir%vimconfig\vimrc"
 
-:: NileSoft Shell
+REM NileSoft Shell
 CALL :Backup "C:\Program Files\NileSoft Shell\" "shell.nss" "Nilesoft Shell"
 mklink "C:\Program Files\NileSoft Shell\shell.nss" "%repo_dir%shell.nss"
 
-:: delete backup folder if nothing was backed up
+REM delete backup folder if nothing was backed up
 dir /b /s /a "%home_dir%old_dotfiles" | findstr .>nul || (
     rmdir "%home_dir%old_dotfiles"
 )
 
-:: FUNCTIONS
+REM ================
+REM END OF MAIN CODE
+REM ================
+
+REM FUNCTIONS
+REM =========
+
 EXIT /B %ERRORLEVEL%
-:: Backup file or directory to backup folder
+REM Backup file or directory to backup folder
 :Backup
 set file_dir=%~1
 set filename=%~2
 set backup_path=%~3
-:: delete previous symbolic link if it exists
+REM delete previous symbolic link if it exists
 dir "%file_dir%*" | find "<SYMLINKD>" | find "%filename%" && (
     rmdir "%file_dir%%filename%"
 )
 dir "%file_dir%*" | find "<SYMLINK>" | find "%filename%" && (
     del "%file_dir%%filename%"
 )
-:: backup the previous file/folder if it exists
+REM backup the previous file/folder if it exists
 if exist "%file_dir%%filename%"  (
     mkdir "%backup_dir%%backup_path%"
     move /-y "%file_dir%%filename%" "%backup_dir%%backup_path%"
