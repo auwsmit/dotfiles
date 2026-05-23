91da57b97955d9e110b532613e765a60aa205180
Austin W Smith <auwsmit@gmail.com>
2026-05-22 21:55:51 -0700

    updated vimrc and blink plugin config

--- vimconfig/plugin_configs/blink.cmp.lua
+++ vimconfig/plugin_configs/blink.cmp.lua
@@ -2,33 +2,9 @@ require('blink.cmp').setup({
     -- no command line blink
     cmdline = { enabled = false },
 
-    -- set maps for supertab compatibility
     keymap = {
-        -- set to 'none' to disable the 'default' preset
-        preset = 'default',
-
-        ['<C-space>'] = { 'show', 'show_documentation', 'hide_documentation' },
-        ['<C-e>'] = { 'hide', 'fallback' },
-
-        ['<Tab>'] = {
-            function(cmp)
-                if cmp.snippet_active() then return cmp.accept()
-                else return cmp.select_and_accept() end
-            end,
-            'snippet_forward',
-            'fallback'
-        },
-        ['<S-Tab>'] = { 'snippet_backward', 'fallback' },
-
-        ['<Up>'] = { 'select_prev', 'fallback' },
-        ['<Down>'] = { 'select_next', 'fallback' },
-        ['<C-p>'] = { 'select_prev', 'fallback_to_mappings' },
-        ['<C-n>'] = { 'select_next', 'fallback_to_mappings' },
-
-        ['<C-b>'] = { 'scroll_documentation_up', 'fallback' },
-        ['<C-f>'] = { 'scroll_documentation_down', 'fallback' },
-
-        ['<C-k>'] = { 'show_signature', 'hide_signature', 'fallback' },
+        -- set maps to be similar to and compatible with supertab
+        preset = 'super-tab',
     },
 
     appearance = {
@@ -38,9 +14,10 @@ require('blink.cmp').setup({
     completion = {
         documentation = { auto_show = false },
         trigger = { show_on_keyword = false },
+
         -- menu = {
         --     draw = {
-        --         -- You can customize the components to hide icons
+        --         -- hide icons
         --         columns = { { "label", "label_description", gap = 1 } },
         --     },
         -- },
